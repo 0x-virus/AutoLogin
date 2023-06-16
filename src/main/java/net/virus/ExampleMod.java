8b85ff4a72c1b32a1327b253784aba911fed034e
Sergey <suhovserg36@yandex.ru>
2023-06-16 22:30:58 +0300

    remove useless code

--- src/main/java/net/virus/ExampleMod.java
+++ src/main/java/net/virus/ExampleMod.java
@@ -2,20 +2,13 @@ package net.virus;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.MinecraftClient;
 
-import net.minecraft.client.network.ServerInfo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Arrays;
 import java.util.List;
 
 
 public class ExampleMod implements ModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("autologin");
 	public static boolean login = true;
 	public static MinecraftClient MC = MinecraftClient.getInstance();
 	public static ModConfig config = ModConfig.getInstance();
